c18ae6854fd1dc3e465f649cbe0304f57d8a3fb1
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-10-29 00:10:50 +0300

    GUIStyle_Apply_Replace

---
+++ CSharp/Client/BarotraumaClient/ClientSource/GUI/GUIStyle.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Barotrauma;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+
+
+namespace CleanPatches
+{
+  public partial class Mod : IAssemblyPlugin
+  {
+    // https://github.com/evilfactory/LuaCsForBarotrauma/blob/master/Barotrauma/BarotraumaClient/ClientSource/GUI/GUIStyle.cs#L204
+    public static bool GUIStyle_Apply_Replace(GUIComponent targetComponent, Identifier styleName, GUIComponent parent)
+    {
+      GUIComponentStyle componentStyle;
+      if (parent != null)
+      {
+        GUIComponentStyle parentStyle = parent.Style;
+
+        if (parentStyle == null)
+        {
+          Identifier parentStyleName = ReflectionUtils.GetTypeNameWithoutGenericArity(parent.GetType());
+          if (!GUIStyle.ComponentStyles.ContainsKey(parentStyleName))
+          {
+            DebugConsole.ThrowError($"Couldn't find a GUI style \"{parentStyleName}\"");
+            return false;
+          }
+          parentStyle = GUIStyle.ComponentStyles[parentStyleName];
+        }
+        Identifier childStyleName = styleName.IsEmpty ? ReflectionUtils.GetTypeNameWithoutGenericArity(targetComponent.GetType()) : styleName;
+        parentStyle.ChildStyles.TryGetValue(childStyleName, out componentStyle);
+      }
+      else
+      {
+        Identifier styleIdentifier = styleName.ToIdentifier();
+        if (styleIdentifier == Identifier.Empty)
+        {
+          styleIdentifier = ReflectionUtils.GetTypeNameWithoutGenericArity(targetComponent.GetType());
+        }
+        if (!GUIStyle.ComponentStyles.ContainsKey(styleIdentifier))
+        {
+          DebugConsole.ThrowError($"Couldn't find a GUI style \"{styleIdentifier}\"");
+          return false;
+        }
+        componentStyle = GUIStyle.ComponentStyles[styleIdentifier];
+      }
+
+      targetComponent.ApplyStyle(componentStyle);
+
+      return false;
+    }
+  }
+}
