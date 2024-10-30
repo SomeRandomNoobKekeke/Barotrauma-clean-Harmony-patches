81ab98174d92282b4bb8ecb986642075e5abce93
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-10-30 11:40:42 +0300

    LevelRenderer_Update_Replace

--- CSharp/Client/BarotraumaClient/ClientSource/Map/Levels/LevelRenderer.cs
+++ CSharp/Client/BarotraumaClient/ClientSource/Map/Levels/LevelRenderer.cs
@@ -30,6 +30,85 @@ namespace CleanPatches
         original: typeof(LevelRenderer).GetMethod("DrawBackground", AccessTools.all),
         prefix: new HarmonyMethod(typeof(Mod).GetMethod("LevelRenderer_DrawBackground_Replace"))
       );
+
+      harmony.Patch(
+        original: typeof(LevelRenderer).GetMethod("Update", AccessTools.all),
+        prefix: new HarmonyMethod(typeof(Mod).GetMethod("LevelRenderer_Update_Replace"))
+      );
+    }
+
+    // https://github.com/evilfactory/LuaCsForBarotrauma/blob/master/Barotrauma/BarotraumaClient/ClientSource/Map/Levels/LevelRenderer.cs#L149
+    public static bool LevelRenderer_Update_Replace(float deltaTime, Camera cam, LevelRenderer __instance)
+    {
+      LevelRenderer _ = __instance;
+
+      if (_.CollapseEffectStrength > 0.0f)
+      {
+        _.CollapseEffectStrength = Math.Max(0.0f, _.CollapseEffectStrength - deltaTime);
+      }
+      if (_.ChromaticAberrationStrength > 0.0f)
+      {
+        _.ChromaticAberrationStrength = Math.Max(0.0f, _.ChromaticAberrationStrength - deltaTime * 10.0f);
+      }
+
+      if (_.level.GenerationParams.FlashInterval.Y > 0)
+      {
+        _.flashCooldown -= deltaTime;
+        if (_.flashCooldown <= 0.0f)
+        {
+          _.flashTimer = 1.0f;
+          if (_.level.GenerationParams.FlashSound != null)
+          {
+            _.level.GenerationParams.FlashSound.Play(1.0f, "default");
+          }
+          _.flashCooldown = Rand.Range(_.level.GenerationParams.FlashInterval.X, _.level.GenerationParams.FlashInterval.Y, Rand.RandSync.Unsynced);
+        }
+        if (_.flashTimer > 0.0f)
+        {
+          float brightness = _.flashTimer * 1.1f - PerlinNoise.GetPerlin((float)Timing.TotalTime, (float)Timing.TotalTime * 0.66f) * 0.1f;
+          _.FlashColor = _.level.GenerationParams.FlashColor.Multiply(MathHelper.Clamp(brightness, 0.0f, 1.0f));
+          _.flashTimer -= deltaTime * 0.5f;
+        }
+        else
+        {
+          _.FlashColor = Color.TransparentBlack;
+        }
+      }
+
+      //calculate the sum of the forces of nearby level triggers
+      //and use it to move the water texture and water distortion effect
+      Vector2 currentWaterParticleVel = _.level.GenerationParams.WaterParticleVelocity;
+      foreach (LevelObject levelObject in _.level.LevelObjectManager.GetVisibleObjects())
+      {
+        if (levelObject.Triggers == null) { continue; }
+        //use the largest water flow velocity of all the triggers
+        Vector2 objectMaxFlow = Vector2.Zero;
+        foreach (LevelTrigger trigger in levelObject.Triggers)
+        {
+          Vector2 vel = trigger.GetWaterFlowVelocity(cam.WorldViewCenter);
+          if (vel.LengthSquared() > objectMaxFlow.LengthSquared())
+          {
+            objectMaxFlow = vel;
+          }
+        }
+        currentWaterParticleVel += objectMaxFlow;
+      }
+
+      _.waterParticleVelocity = Vector2.Lerp(_.waterParticleVelocity, currentWaterParticleVel, deltaTime);
+
+      WaterRenderer.Instance?.ScrollWater(_.waterParticleVelocity, deltaTime);
+
+      if (_.level.GenerationParams.WaterParticles != null)
+      {
+        Vector2 waterTextureSize = _.level.GenerationParams.WaterParticles.size * _.level.GenerationParams.WaterParticleScale;
+        _.waterParticleOffset += new Vector2(_.waterParticleVelocity.X, -_.waterParticleVelocity.Y) * _.level.GenerationParams.WaterParticleScale * deltaTime;
+        while (_.waterParticleOffset.X <= -waterTextureSize.X) { _.waterParticleOffset.X += waterTextureSize.X; }
+        while (_.waterParticleOffset.X >= waterTextureSize.X) { _.waterParticleOffset.X -= waterTextureSize.X; }
+        while (_.waterParticleOffset.Y <= -waterTextureSize.Y) { _.waterParticleOffset.Y += waterTextureSize.Y; }
+        while (_.waterParticleOffset.Y >= waterTextureSize.Y) { _.waterParticleOffset.Y -= waterTextureSize.Y; }
+      }
+
+      return false;
     }
 
 
