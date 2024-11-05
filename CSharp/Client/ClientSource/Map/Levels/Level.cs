e1d5cf3475bb4836f7d8b2e70b1d29ea31b08bf9
SomeRandomNoobKekeke <122838333+SomeRandomNoobKekeke@users.noreply.github.com>
2024-11-05 20:33:26 +0300

    Level_DrawBack_Replace

---
+++ CSharp/Client/ClientSource/Map/Levels/Level.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Barotrauma;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+
+using Barotrauma.Networking;
+using FarseerPhysics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace CleanPatches
+{
+  public partial class Mod : IAssemblyPlugin
+  {
+    [ThisIsHowToPatchIt]
+    public static void PatchClientLevel()
+    {
+      harmony.Patch(
+        original: typeof(Level).GetMethod("DrawBack", AccessTools.all),
+        prefix: new HarmonyMethod(typeof(Mod).GetMethod("Level_DrawBack_Replace"))
+      );
+    }
+
+
+    // https://github.com/evilfactory/LuaCsForBarotrauma/blob/master/Barotrauma/BarotraumaClient/ClientSource/Map/Levels/Level.cs#L124
+    public static bool Level_DrawBack_Replace(GraphicsDevice graphics, SpriteBatch spriteBatch, Camera cam, Level __instance)
+    {
+      Level _ = __instance;
+
+      float brightness = MathHelper.Clamp(1.1f + (cam.Position.Y - _.Size.Y) / 100000.0f, 0.1f, 1.0f);
+      var lightColorHLS = _.GenerationParams.AmbientLightColor.RgbToHLS();
+      lightColorHLS.Y *= brightness;
+
+      GameMain.LightManager.AmbientLight = ToolBox.HLSToRGB(lightColorHLS);
+
+      graphics.Clear(_.BackgroundColor);
+
+      if (_.renderer != null)
+      {
+        GameMain.LightManager.AmbientLight = GameMain.LightManager.AmbientLight.Add(_.renderer.FlashColor);
+        _.renderer?.DrawBackground(spriteBatch, cam, _.LevelObjectManager, _.backgroundCreatureManager);
+      }
+
+      return false;
+    }
+  }
+}
